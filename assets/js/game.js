435b1fd99003fc8abe0dfbcb41c8d62e2cb3d1ba
Melissa414 <Missy41491@gmail.com>
2017-03-14 12:07:40 -0400

    visually finished

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,28 +1,23 @@
-window.onload = function(){
+window.onload = function() {
 
-var score = 0;
+    var score = 0;
 
 
-  $("button").mouseover(function() {
-    var x = Math.floor((Math.random() * 8) + 1);
-      $(this).append(x).off();
+    $("button").mouseover(function() {
+        var x = Math.floor((Math.random() * 6) + 1);
+        $(this).append(x).off();
 
-  $(this).on("click", function(){
-      if(x + x + x === 9){
-        document.getElementById("score").innerHTML= "<h4>Score: </h4>" + score; 
-        score ++;
-      }
-      else{
-        // score --;
-      }
+        $(this).on("click", function() {
+            if (x + x + x === 9) {
+                document.getElementById("score").innerHTML = "<h4>Score: </h4>" + score;
+                score++;
+            } else {
+                // score --;
+            }
+        });
+        $("#restart").on("click", function() {
+            $("button").html(Math.floor(Math.random(x) * 6) + 1);
+              // $(this).append(x);
+        });
     });
-
-  });
-
-  $("#restart").on("click",function(){
-    var y = Math.floor((Math.random() * 7) + 1);
-      $("button").text(y);
-        // $(this).random(y);
-  });
 };
-
