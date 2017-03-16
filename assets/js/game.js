1cfefd862edabfb3e827d3169e0623e0abbf2a7c
Melissa414 <Missy41491@gmail.com>
2017-03-16 07:41:27 -0400

    few minor tweaks

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,16 @@
 window.onload = function() {
 
+    var random = [];
     var score = 0;
 
 
     $("button").mouseover(function() {
-        var x = Math.floor((Math.random() * 6) + 1);
+        var x = Math.floor((Math.random() * 9) + 0);
+        var random = x;
         $(this).append(x).off();
 
         $(this).on("click", function() {
-            if (x + x + x === 9) {
+            if (x + x + x == 9) {
                 document.getElementById("score").innerHTML = "<h4>Score: </h4>" + score;
                 score++;
             } else {
@@ -16,8 +18,8 @@ window.onload = function() {
             }
         });
         $("#restart").on("click", function() {
-            $("button").html(Math.floor(Math.random(x) * 6) + 1);
-              // $(this).append(x);
+            $("button").html(Math.floor(Math.random() * 9) + 0);
+            // $(this).sort(random);
         });
     });
 };
